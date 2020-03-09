149ff0e3b4458fa800315f44861a841631403217
tom <>
2020-03-09 22:06:30 +0800

    added doc strings for create_final_image.py

--- create_final_image.py
+++ create_final_image.py
@@ -2,14 +2,20 @@ from PIL import Image
 
 
 def create_final_image(solution_path, input_path):
+	"""
+	Void function that marks the solution path into the image with green and saves the image
+
+	:param solution_path: list of coordinates to the cells in the solution
+	:param input_path: User-supplied path to input maze image
+	"""
 	solution_image = Image.open(input_path)
-	width, height = solution_image.size
 
 	for i in solution_path:
-		solution_image.putpixel((i[1], i[0]), (255,0,0))
+		solution_image.putpixel((i[1], i[0]), (0,255,0))
 
 	image_name = input_path.split("/")[-1]
 	image_name = image_name.split(".")[0]
 
 	solution_image.save(f"./pics/{image_name}_out.jpg", subsampling=0, quality=100)
+
 
