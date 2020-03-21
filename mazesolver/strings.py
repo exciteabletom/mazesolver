007d8d3ee703b493d5cb8cba3522d8904070c67b
Tommy Dougiamas <tom@digitalnook.net>
2020-03-21 19:29:27 +0800

    Update strings.py

--- mazesolver/strings.py
+++ mazesolver/strings.py
@@ -36,5 +36,5 @@ What are the rules for maze images?
 Check out some of the example mazes at https://github.com/exciteabletom/mazesolver/tree/master/pics \
 """
 
-version = "1.2.5.post1"
+version = "1.2.5.post2"
 version_long = f"mazesolver{version}"
