37f8843c0f0029f3e73ed01d3e735f8794eb44ee
exciteabletom <tom@digitalnook.net>
2020-03-23 19:39:31 +0800

    Removed some global variables that were no longer being used

--- mazesolver/g.py
+++ mazesolver/g.py
@@ -3,11 +3,5 @@
 
 # Matrix representing the maze image
 # Set in __main__.py
-# Used in __solve.py
+# Used in solve.py
 maze = []
-
-# Boolean storing whether we are on Windows or not
-# It's set at the beginning of __main__.py and is used there
-# It's also used in create_final_image.py
-is_windows = False
-file_delimiter = "/"
